8a79b86d9f62b5e93574214871c9e6662fb8cbc8
seyun kim <>
2024-11-19 11:03:53 +0900

    FEAT : 초기세팅

---
+++ DrinkDilemma/src/main/java/com/example/DrinkDilemma/DrinkDilemmaApplication.java
@@ -0,0 +1,13 @@
+package com.example.DrinkDilemma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkDilemmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkDilemmaApplication.class, args);
+	}
+
+}
---
+++ DrinkDilemma/src/test/java/com/example/DrinkDilemma/DrinkDilemmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DrinkDilemma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkDilemmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
