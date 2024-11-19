50b4b7d5305874eeaa24fa40fca4015cceeba2f7
seyun kim <>
2024-11-19 11:02:27 +0900

    FEAT : Menu 테이블 추가

---
+++ DrinkDilemma/src/main/java/com/example/DrinkDilemma/domain/Menu.java
@@ -0,0 +1,25 @@
+package com.example.DrinkDilemma.domain;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "menu")
+@ToString
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idx;
+
+    private String menuName;
+    private String price;
+
+    @ManyToOne
+    @JoinColumn(name = "cafe_id")
+    private Cafe cafe;
+}
